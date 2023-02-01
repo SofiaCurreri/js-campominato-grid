9671b3e4ec9027edc4fc6e6c4089141a0e01fb72
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-01 16:53:06 +0100

    creazione button e griglia

--- js/main.js
+++ js/main.js
@@ -2,3 +2,45 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+
+/**************************************************************************
+*                                                                         *
+*                                 ON LOAD                                 *
+*                                                                         *
+***************************************************************************/
+let playButton = document.getElementById("play");
+
+
+
+/**************************************************************************
+ *                                                                         *
+ *                              EVENT LISTENER                             *
+ *                                                                         *
+***************************************************************************/
+playButton.addEventListener(
+    "click",
+    function () {
+        const gridEl = document.getElementById("grid");
+        const dimension = 100;
+        createGrid(gridEl, dimension);
+    }
+);
+
+
+
+/**************************************************************************
+*                                                                         *
+*                                 FUNCTIONS                               *
+*                                                                         *
+***************************************************************************/
+function createGrid(gridEl, dim) {
+    gridEl.innerHTML = "";
+    for (let i = 0; i < dim; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        gridEl.append(square);
+    }
+}
