2308b6762da9e61caea73171703995b78f814f77
SofiaCurreri <sofiacurreri@gmail.com>
2023-02-01 17:57:52 +0100

    select (1a parte bonus)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
-
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
 /**************************************************************************
@@ -12,6 +16,7 @@
 *                                                                         *
 ***************************************************************************/
 let playButton = document.getElementById("play");
+const playLevel = document.getElementById("mySelect");
 
 
 
@@ -24,7 +29,16 @@ playButton.addEventListener(
     "click",
     function () {
         const gridEl = document.getElementById("grid");
-        const dimension = 100;
+        const chooseLevel = playLevel.value;
+        let dimension;
+
+        if (chooseLevel == "l-1") {
+            dimension = 100;
+        } else if (chooseLevel == "l-2") {
+            dimension = 81;
+        } else {
+            dimension = 49;
+        }
         createGrid(gridEl, dimension);
     }
 );
